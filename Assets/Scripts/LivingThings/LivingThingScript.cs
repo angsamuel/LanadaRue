a5759473313aab52165911df237c2ad5de9ba733
astralsquid <samuel.ang.prog@gmail.com>
2016-10-19 01:36:33 -0500

    Decided on 32 x 32

---
+++ Assets/Scripts/LivingThings/LivingThingScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LivingThingScript : MonoBehaviour {
+	private string name;
+	private string gender;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//GET FUNCTIONS
+	public string GetName(){return name;}
+	public string GetGender(){return gender;}
+}
---
+++ Assets/Scripts/Player/PlayerInputControllerScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInputControllerScript : MonoBehaviour {
+	GameObject playerCharacterObject;
+	string mode;
+	// Use this for initialization
+	void Start () {
+		//link player character to input controller
+		mode = "movement";
+		playerCharacterObject = GameObject.Find ("PlayerCharacter");
+	}
+	
+	
+}
