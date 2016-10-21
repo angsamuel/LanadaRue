39741f9690305e0c1000ebe7cf00a9f6af54b2de
astralsquid <samuel.ang.prog@gmail.com>
2016-10-21 15:53:38 -0500

    Added Basic Player Movement

--- Assets/Scripts/LivingThings/LivingThingScript.cs
+++ Assets/Scripts/LivingThings/LivingThingScript.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class LivingThingScript : MonoBehaviour {
 	private string name;
 	private string gender;
+    int posX;
+    int posY;
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +16,13 @@ public class LivingThingScript : MonoBehaviour {
 	void Update () {
 	
 	}
+    protected void Move(int x, int y)
+    {
+        posX += x;
+        posY += y;
+        transform.Translate(new Vector3(x, y, -1));
+
+    }
 
 	//GET FUNCTIONS
 	public string GetName(){return name;}
--- Assets/Scripts/Player/PlayerCharacterScript.cs
+++ Assets/Scripts/Player/PlayerCharacterScript.cs
@@ -5,7 +5,12 @@ public class PlayerCharacterScript : HumanScript {
 	//position on level grid
 	private int posX;
 	private int posY;
-	private LevelControllerScript levelControllerScript;
+
+    private bool canMoveUp = true;
+    private bool canMoveDown = true;
+    private bool canMoveLeft = true;
+    private bool canMoveRight = true;
+    private LevelControllerScript levelControllerScript;
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +19,22 @@ public class PlayerCharacterScript : HumanScript {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        ScanForMovementInputs();
+    }
+
+    void ScanForMovementInputs()
+    {
+
+            if (Input.GetAxisRaw("MoveUp") != 0 && canMoveUp) { Move(0, 1); canMoveUp = false; }
+            if (Input.GetAxisRaw("MoveDown") != 0 && canMoveDown) { Move(0, -1); canMoveDown = false; }
+            if (Input.GetAxisRaw("MoveLeft") != 0 && canMoveLeft) { Move(-1, 0); canMoveLeft = false; }
+            if (Input.GetAxisRaw("MoveRight") != 0 && canMoveRight) { Move(1, 0); canMoveRight = false; }
+
+            if (Input.GetAxisRaw("MoveUp") == 0) { canMoveUp = true; }
+            if (Input.GetAxisRaw("MoveDown") == 0) {canMoveDown = true; }
+            if (Input.GetAxisRaw("MoveLeft") == 0) {canMoveLeft = true; }
+            if (Input.GetAxisRaw("MoveRight") == 0) {canMoveRight = true; }
+
+      
+    }
 }
